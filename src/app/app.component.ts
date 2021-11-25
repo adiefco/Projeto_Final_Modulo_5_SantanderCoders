b8740a118cc5c150485e4a030ea327376766a886
edwedja <edwedja_lima@hotmail.com>
2021-11-24 21:47:00 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto-final-modulo5';
+
+  firstblock = {
+    title: "Aproveite na TV.",
+    history: "Assista em Smart TVs, Playstation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray e outros dispositivos.",
+    image: "./assets/images/1tv.png",
+    width:"570",
+    image2:"./assets/images/netflix.png",
+    width2: " 238",
+    ordem: "true",
+  }
+
+  secondblock = {
+    image: "./assets/images/2mobile.png",
+    width: "535",
+    title: "Baixe séries para assistir offline.",
+    history: "alve seus títulos favoritos e sempre tenha algo para assistir.",
+    
+  }
+  
+  thirdblock = {
+    title: "Assista quando quiser.",
+    history: "Assista no celular, tablet, Smart TV ou notebook sem pagar a mais por isso.",
+    image: "./assets/images/3tv_2.png",
+    width:"568 ",
+    ordem: "true",
+  }
+    
+  fourthblock= {
+    image: "./assets/images/4infantil.png",
+    width:"514" ,
+    title: "Crie perfis para crianças",
+    
+  }
+   
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerComponentComponent } from './banner-component/banner-component.component';
+import { BlocksComponent } from './blocks/blocks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponentComponent
+    BannerComponentComponent,
+    BlocksComponent
   ],
   imports: [
     BrowserModule,
