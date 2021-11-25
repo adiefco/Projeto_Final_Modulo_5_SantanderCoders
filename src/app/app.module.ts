a14c94ac28c87e19d4d3e7936906eb532d367f1f
GenilsonCavalcante <cavalcantedeoliveiragenilson@gmail.com>
2021-11-25 16:00:00 -0300

    Create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Frame4Component } from './frame4/frame4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Frame4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/frame4/frame4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frame4',
+  templateUrl: './frame4.component.html',
+  styleUrls: ['./frame4.component.scss']
+})
+export class Frame4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
